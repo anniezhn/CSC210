76856088c440f495a5675f5d52071a441f9843ea
Tom Nichols <thnichols@gmail.com>
2014-10-19 16:13:40 -0400

    basics2.py
    Upload of the second lesson script file

---
+++ basics2.py
@@ -0,0 +1,22 @@
+#!/user/bin/python
+
+import cgi
+import sys
+import cgitb
+cgitb.enable() #catch Python errors
+form = cgi.FieldStorage()
+
+#get username/account information from cookie when enabled
+#possibly save progress somehow in database?
+
+print "Content-type: text/html"
+print #don't forget the extra newline!
+print "<html>"
+
+print "<head><title>TAJ: Basics 2</title></head>"
+print "<body>"
+print "<h1>Second Lesson: </h1>"
+print "<h2>Definitions: </h2>"
+print "<p><strong>Conditional Statements:</strong> </p>"
+print "</body>"
+print "</html>"
