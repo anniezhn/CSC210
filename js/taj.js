c2612266da649975e1e83d33622b2c0ce4053475
Tom Nichols <thnichols@gmail.com>
2014-10-23 17:32:50 -0400

    JS login check take 2

--- js/taj.js
+++ js/taj.js
@@ -1,6 +1,8 @@
-jQuery.ajax({
-    url: "http://tnichols.rochestercs.org/cgi-bin/loginCheck.py",
-    method: "GET",
-    dataType: "text",
-    success: function () {}
+jQuery(document).ready(function ($) {
+    $.ajax({
+        url: "http://tnichols.rochestercs.org/cgi-bin/loginCheck.py",
+        method: "GET",
+        dataType: "text",
+        success: function () {}
+    });
 });
