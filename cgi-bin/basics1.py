70a4bd776d7dc81340ee8e632d1dbe47b7121553
Annie Zhang <annie.zhn@gmail.com>
2014-10-24 11:22:03 -0400

    maybe?

--- cgi-bin/basics1.py
+++ cgi-bin/basics1.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python
 
-import cgi
-import sys
-import cgitb
-cgitb.enable() #catch Python errors
-form = cgi.FieldStorage()
-
-#get username/account information from cookie when enabled
-#possibly save progress somehow in database?
-
-print 'Content-type: text/plain'
+print "Content-type: text/plain"
 print
 print 'A string variable is any combination of letters or numbers contained within quotes. For example, 'Hello World!' is a string and so is 'abc123'.'
