d07f8607c11d4b111187765e553832f9503014db
Annie Zhang <annie.zhn@gmail.com>
2014-10-24 11:37:57 -0400

    Changed things around a bit

---
+++ js/basics1.js
@@ -0,0 +1,30 @@
+ jQuery(document).ready(function($) {
+	  console.log("Test");
+	  <!--> I found this click once function online but it's not working.
+	  $("#getStringDef").one('click', function() {
+	        $.ajax({
+	           url: "cgi-bin/basics1.py",
+	           type: "GET",
+	           dataType: "text",
+	           success: function(dat) {
+	             console.log(dat);
+	             $("#string").append('<li>' + dat + '</li>');
+	           },
+	        });
+	      });
+</-->
+    $("#getStringDef").click(function() {
+    $.ajax(
+      {
+        url: "cgi-bin/basics1.py",
+        type: "GET",
+        dataType: "text",
+
+        success: function(dat) {
+          console.dir(dat);
+          $("#string").append(dat);
+        },
+      }
+    );
+  });
+});
