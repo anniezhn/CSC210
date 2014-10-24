156abdae84bfa03d2d96f4e80d8eae5bf7aaa720
Annie Zhang <annie.zhn@gmail.com>
2014-10-24 02:57:40 -0400

    Starting Lesson 1

--- cgi-bin/basics1.py
+++ cgi-bin/basics1.py
@@ -8,15 +8,3 @@ form = cgi.FieldStorage()
 
 #get username/account information from cookie when enabled
 #possibly save progress somehow in database?
-
-print "Content-type: text/html"
-print #don't forget the extra newline!
-print "<html>"
-
-print "<head><title>TAJ: Basics 1 </title></head>"
-print "<body>"
-print "<h1>First Lesson: </h1>"
-print "<h2>Definitions: </h2>"
-print "<p><strong>Variables:</strong> </p>"
-print "</body>"
-print "</html>"
