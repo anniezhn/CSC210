2b8722042e6827bcbedc18392bff9def358941be
Tom Nichols <thnichols@gmail.com>
2014-10-23 21:19:10 -0400

    Trying return True/False in login check

--- cgi-bin/loginCheck.py
+++ cgi-bin/loginCheck.py
@@ -14,10 +14,10 @@ def main(): #return True if there's a cookie and false if not
 	#print "Hey there!"
 	if 'HTTP_COOKIE' in os.environ:
 		print "COOKIE"
-		sys.exit(0) #if this does not work, substitute "return True"
+		return True #sys.exit(0) #if this does not work, substitute "return True"
 	else:
 		print "NO COOKIE"
-		sys.exit(1) #if this does not work, substitute "return False"
+		return False #sys.exit(1) #if this does not work, substitute "return False"
 
 if __name__ == "__main__":
 	main()
