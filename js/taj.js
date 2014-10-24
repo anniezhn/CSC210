1ecb5df3b3f57d6ab327daa24bbfac6920ce207a
Tom Nichols <thnichols@gmail.com>
2014-10-23 21:50:11 -0400

    More taj.js testing

--- js/taj.js
+++ js/taj.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function ($) {
         method: "GET",
         dataType: "text",
         success: function (data) {
-            if (data.toString() === "true") {
+            if (data == "true") {
                 window.location.href = "http://tnichols.rochestercs.org/homepage.html";
             } else {
                 console.log(data);
