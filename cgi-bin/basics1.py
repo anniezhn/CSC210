3c1e5fcb5d2a8716fe1331bc298148450a05e417
Annie Zhang <annie.zhn@gmail.com>
2014-10-24 03:28:31 -0400

    Lesson 1!!

--- cgi-bin/basics1.py
+++ cgi-bin/basics1.py
@@ -8,3 +8,7 @@ form = cgi.FieldStorage()
 
 #get username/account information from cookie when enabled
 #possibly save progress somehow in database?
+
+print "Content-type: text/plain"
+print
+print 'A string variable is any combination of letters or numbers contained within quotes. For example, "Hello World!" is a string and so is "abc123".'
