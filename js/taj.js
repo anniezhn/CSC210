59973dcbb147d88af0226de34d3314baa97882fa
Tom Nichols <thnichols@gmail.com>
2014-10-23 20:40:39 -0400

    Login check JS update
    Added some error reporting to the Ajax request. Also created space for a
    bigger nav bar.

--- js/taj.js
+++ js/taj.js
@@ -3,6 +3,21 @@ jQuery(document).ready(function ($) {
         url: "http://tnichols.rochestercs.org/cgi-bin/loginCheck.py",
         method: "GET",
         dataType: "text",
-        success: function () {}
+        success: function (data) {
+            if (data === true) {
+                window.location.href = "http://tnichols.rochestercs.org/homepage.html";
+            }
+        },
+        error: function (jqXHR, errorStatus, errorString) {
+            $("body").prepend(
+                "<div>" +
+                "<ul>" +
+                    "<li>Error: " + errorStatus + "</li>" +
+                    "<li>Message: " + errorString + "</li>" + 
+                "</ul>" +
+                "</div>" +
+                "<div>Something went wrong.</div>"
+            );
+        }
     });
 });
