dc1ca58e5bbd3126a69f060e1adc98c9d46add62
Annie Zhang <annie.zhn@gmail.com>
2014-11-14 15:47:19 -0500

    Quiz

--- cgi-bin/basics1/basics1-array.py
+++ cgi-bin/basics1/basics1-array.py
@@ -2,4 +2,4 @@
 
 print "Content-type: text/plain"
 print
-print 'An <b>array</b> is basically a list of other variables in some order. For example: 1,4,5,2 can be stored as a array of length 4 containing integer variables. Arrays let us have collections of variables and save them in that collection. In fact, a string can also be considered an array of characters.'
+print 'An <b>array</b> is basically a list of other variables in some order. For example: 1,4,5,2 can be stored as an array of length 4 containing integer variables. Arrays let us have collections of variables and save them in that collection. In fact, a string can also be considered an array of characters.'
