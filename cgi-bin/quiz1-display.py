cc992f6c01c5cd826548307648f83f33698fe0ae
Annie Zhang <annie.zhn@gmail.com>
2014-11-26 18:17:22 -0500

    Quiz print out

--- cgi-bin/quiz1-display.py
+++ cgi-bin/quiz1-display.py
@@ -25,30 +25,19 @@ try:
    		cur.execute('select Score1 from Quiz1 where UserID=%s',userID)
 		score1 = str(cur.fetchone()[0])
 		if score1 != Null: #if there is a quiz grade in the database.
-			print 'Content-type: text/html'
+			print 'Content-type: text/plain'
 			print
-			print '<html>'
-			print '<body>'
 			print "You have already taken Quiz 1 and received" + score1 + ""
-			print '</body>'
-			print '</html>'
 		else:
-			print 'Content-type: text/html'
+			print 'Content-type: text/plain'
 			print
-			print '<html>'
-			print '<body>'
 			print "You have not passed the first lesson yet"
 			print '</body>'
-			print '</html>'
 
 except mbd.Error, e:
-	print 'Content-type: text/html'
+	print 'Content-type: text/plain'
 	print
-	print '<html>
-	print "<body>"
 	print "Error %d: %s" % (e.args[0], e.args[1])
-	print "</body>"
-	print "</html>"
 	sys.exit(1)
 finally:
 	if conn:
