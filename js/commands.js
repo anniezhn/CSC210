5c726ff584278db7060a4153d668a1c678b3d796
Annie Zhang <annie.zhn@gmail.com>
2014-12-05 03:41:52 -0500

    Fixed the effect call

--- js/commands.js
+++ js/commands.js
@@ -1,17 +1,7 @@
 jQuery(document).ready(function ($) { 
-    // callback function to bring a hidden box back
-    function callback() {
-      setTimeout(function() {
-        $( "#effect" ).removeAttr( "style" ).hide().fadeIn();
-      }, 1000 );
-    };
- 
     $( "#run" ).click(function() {
-      $( "#effect" ).effect("bounce", {}, 500, callback());
-      return false;
+      $( "#effect" ).effect("bounce", {times: 5}, 300);
     });
   
-  
-  
-  
+
 });
