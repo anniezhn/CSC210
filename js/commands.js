dc69179aedb7f3aceacc26e22ccf96a44cef0c7c
Annie Zhang <annie.zhn@gmail.com>
2014-12-05 03:23:28 -0500

    Added a command to commands lesson

---
+++ js/commands.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function ($) { 
+    // callback function to bring a hidden box back
+    function callback() {
+      setTimeout(function() {
+        $( "#effect" ).removeAttr( "style" ).hide().fadeIn();
+      }, 1000 );
+    };
+ 
+    $( "#run" ).click(function() {
+      $( "#effect" ).effect(bounce, {}, 500)
+      return false;
+    });
+  
+  
+  
+  
+});
