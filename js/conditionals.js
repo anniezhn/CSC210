30b63e033feb2dc4a835e0a91d883f7ba0eea933
Tom Nichols <thnichols@gmail.com>
2014-12-08 16:03:26 -0500

    Adding conditionals exercise 2
    Plus updating styles

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,13 +1,25 @@
 jQuery(document).ready(function ($) {
+  sessionStorage.setItem("exercise", "1");
   $("#x1").droppable({ scope: "cond1" });
-  $(".x1Choice").draggable({ scope: "cond1", containment: $("#condExercises") });
+  $(".xChoice").draggable({ scope: "cond1", containment: $("#condExercises") });
   $("#x1").on("drop", function (event, ui) {
     var compareInt = 5,
         droppedVal = parseInt(ui.draggable.text());
     $(".condResult").html("<p>Is " + droppedVal + " > " + compareInt + "?</p><p> " +
       (droppedVal > compareInt).toString() + "</p>");
+    if (droppedVal > compareInt) {
+      $(".nextExercise").show();
+    }
   });
   $("#x1").on("dropout", function (event, ui) {
     $(".condResult").html("");
+  });
+  $(".nextExercise").on("click", function (event) {
+    var current = parseInt(sessionStorage.getItem("exercise")),
+        next = current + 1;
+    $("#ex" + current.toString()).fadeOut(function () {
+      $("#ex" + next.toString()).fadeIn();
+    });
+    sessionStorage.setItem("exercise", next.toString());
   });
 });
