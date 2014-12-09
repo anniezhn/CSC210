7060e7ce695813bcfb6e228ff3aa4255d0b2a2bd
Tom Nichols <thnichols@gmail.com>
2014-12-08 20:20:37 -0500

    Conditionals Ex. 3

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function ($) {
   sessionStorage.setItem("exercise", "1");
+
   $("#x1").droppable({ scope: "cond1" });
   $(".x1Choice").draggable({ scope: "cond1", containment: $("#condExercises") });
   $("#x1").on("drop", function (event, ui) {
     var compareInt = 5,
         droppedVal = parseInt(ui.draggable.text());
     $(".condResult").html("<p>Is " + droppedVal + " > " + compareInt + "?</p><p> " +
-      (droppedVal > compareInt).toString() + "</p>");
+      (droppedVal > compareInt).toString() + "</p>").show();
     if (droppedVal > compareInt) {
       $(".nextExercise").show();
     }
@@ -14,13 +15,14 @@ jQuery(document).ready(function ($) {
   $("#x1").on("dropout", function (event, ui) {
     $(".condResult").html("");
   });
+
   $("#x2").droppable({ scope: "cond2" });
   $(".x2Choice").draggable({ scope: "cond2", containment: $("#condExercises") });
   $("#x2").on("drop", function (event, ui) {
     var compareInt = 25,
         droppedVal = parseInt(ui.draggable.text());
     $(".condResult").html("<p>Is !(" + droppedVal + " < " + compareInt + ")?</p><p> " +
-      (!(droppedVal < compareInt)).toString() + "</p>");
+      (!(droppedVal < compareInt)).toString() + "</p>").show();
     if (!(droppedVal < compareInt)) {
       $(".nextExercise").show();
     }
@@ -28,6 +30,31 @@ jQuery(document).ready(function ($) {
   $("#x2").on("dropout", function (event, ui) {
     $(".condResult").html("");
   });
+
+  $("#x3 #y3").droppable({ scope: "cond3" });
+  $(".x3Choice").draggable({ scope: "cond3", containment: $("#condExercises") });
+  $("#x3 #y3").on("drop", function (event, ui) {
+    var compareInt = 0,
+        droppedXVal = 1000,
+        droppedYVal = 1000;
+    if (this.id === "x3") {
+      droppedXVal = parseInt(ui.draggable.text());
+    } else if (this.id === "y3") {
+      droppedYVal = parseInt(ui.draggable.text());
+    }
+    if (droppedXVal !== 1000 and droppedYVal !== 1000) {
+      $(".condResult").html("<p>Is " + droppedXVal + " < " droppedYVal + " && " +
+        droppedYVal + " < " + compareInt + "?</p>" +
+        (droppedXVal < droppedYVal && droppedYVal < compareInt).toString()).show();
+      if (droppedXVal < droppedYVal && droppedYVal < compareInt) {
+        $(".nextExercise").show();
+      }
+    }
+  });
+  $("#x3 #y3").on("dropout", function (event, ui) {
+    $(".condResult").html("");
+  });
+
   $(".nextExerciseB").on("click", function (event) {
     var current = parseInt(sessionStorage.getItem("exercise")),
         next = current + 1;
