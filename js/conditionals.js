fc25a538f0c07cf89a78b0c8a4e806bd77d95919
Tom Nichols <thnichols@gmail.com>
2014-12-08 19:01:42 -0500

    Conditionals Ex. 2

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,8 +1,7 @@
 jQuery(document).ready(function ($) {
   sessionStorage.setItem("exercise", "1");
-  console.log(sessionStorage.getItem("exercise"));
   $("#x1").droppable({ scope: "cond1" });
-  $(".xChoice").draggable({ scope: "cond1", containment: $("#condExercises") });
+  $(".x1Choice").draggable({ scope: "cond1", containment: $("#condExercises") });
   $("#x1").on("drop", function (event, ui) {
     var compareInt = 5,
         droppedVal = parseInt(ui.draggable.text());
@@ -15,16 +14,30 @@ jQuery(document).ready(function ($) {
   $("#x1").on("dropout", function (event, ui) {
     $(".condResult").html("");
   });
+  $("#x2").droppable({ scope: "cond2" });
+  $(".x2Choice").draggable({ scope: "cond2", containment: $("#condExercises") });
+  $("#x2").on("drop", function (event, ui) {
+    var compareInt = 25,
+        droppedVal = parseInt(ui.draggable.text());
+    $(".condResult").html("<p>Is !(" + droppedVal + " < " + compareInt + ")?</p><p> " +
+      (!(droppedVal < compareInt)).toString() + "</p>");
+    if (!(droppedVal < compareInt)) {
+      $(".nextExercise").show();
+    }
+  });
+  $("#x2").on("dropout", function (event, ui) {
+    $(".condResult").html("");
+  });
   $(".nextExerciseB").on("click", function (event) {
     var current = parseInt(sessionStorage.getItem("exercise")),
         next = current + 1;
-    console.log("current = " + current);
-    console.log("next = " + next);
-    $("#ex" + current.toString()).fadeOut(function () {
-      $("#ex" + next.toString()).fadeIn();
-    });
-    sessionStorage.setItem("exercise", next.toString());
-    $(".nextExercise").hide();
-    $(".condResult").html("").hide();
+    if (next > 1 && next < 4) {
+      $("#ex" + current.toString()).fadeOut(function () {
+        $("#ex" + next.toString()).fadeIn();
+      });
+      sessionStorage.setItem("exercise", next.toString());
+      $(".nextExercise").hide();
+      $(".condResult").html("").hide();
+    }
   });
 });
