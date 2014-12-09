8a01c00ce29299d76f13b04754d3bb646620d92e
Annie Zhang <annie.zhn@gmail.com>
2014-12-09 16:12:28 -0500

    Add another exercise

--- js/commands.js
+++ js/commands.js
@@ -1,6 +1,34 @@
 jQuery(document).ready(function ($) { 
+	// to save what exercise they are on
+	sessionStorage.setItem("exercise", "1");
+	
+	// runs the first exercise, shakes the box
     $( "#run" ).click(function() {
-      $( "#effect" ).effect("shake", {times: 5}, 300);
+      $( "#effect" ).effect("shake", {times: 5}, 300),
+      $(".nextExercise").show();
     });
 
+	// runs the second exercise, printing to screen
+	$("#runProgram").submit(function(event) {
+		event.preventDefault(),
+		var txt = $("#print").val(),
+		$("#output").html("<p>" + txt + "</p>"),
+		$(".nextExercise").show();
+		});
+		
+		
+	// Show the next exercise
+(".nextExerciseB").on("click", function (event) {
+    var current = parseInt(sessionStorage.getItem("exercise")),
+        next = current + 1;
+    if (next > 1 && next <= 3) {
+      $("#comm" + current.toString()).fadeOut(function () {
+        $("#comm" + next.toString()).fadeIn();
+      });
+      sessionStorage.setItem("exercise", next.toString());
+      $(".nextExercise").hide();
+      $(".condResult").html("").hide();
+    }
+  });
+  
 });
